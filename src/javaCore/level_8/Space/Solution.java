77b2b2ca15b90b9023974def3bfbe7517ff722d6
Mordyasov Alex <mordyasov.60@mail.ru>
2020-05-04 17:39:41 +0700

    new task - Space

---
+++ src/javaCore/level_8/Space/Solution.java
@@ -0,0 +1,34 @@
+package javaCore.level_8.Space;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		try(BufferedReader buffRead = new BufferedReader(new FileReader(args[0]))){
+			String line;
+			int countSpace = 0;
+			int allSymbols = 0;
+			
+			while((line = buffRead.readLine()) != null) {
+				allSymbols += line.length();
+				if(line.contains(" ")) {
+					while(true) {
+						countSpace++;
+						line = line.substring(line.indexOf(' ') + 1);
+						if(!line.contains(" ")) break;
+					}
+				}
+			}
+			System.out.printf("%.2f", ((float)countSpace / allSymbols * 100));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
