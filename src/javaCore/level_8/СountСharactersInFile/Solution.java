52b0ac9220694bd2be847c1016359949238a0cd1
Alexo <mordyasov.60@mail.ru>
2020-05-05 19:47:59 +0700

    perfectionism

--- src/javaCore/level_8/СountСharactersInFile/Solution.java
+++ src/javaCore/level_8/СountСharactersInFile/Solution.java
@@ -1,9 +1,14 @@
 package javaCore.level_8.—ount—haractersInFile;
 
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+
+/*
+ * Counting characters in a file.
+ */
 
 public class Solution {
 
@@ -11,10 +16,12 @@ public class Solution {
 		try(FileReader fileReader = new FileReader(args[0])){
 			TreeMap<Character, Integer> map = new TreeMap<>();
 			int ch;
+			
 			while((ch = fileReader.read()) != -1) 
-					map.merge((char)ch, 1, (a, b) -> (a+1));	
+				map.merge((char)ch, 1, (a, b) -> (a+1));	
 			
 			map.forEach((k, v) -> System.out.println((char)k + " " + v));
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
