5b2a3a95a9a151e0dcbff0cd46369cbf284e51ea
Alexo <mordyasov.60@mail.ru>
2020-05-05 19:44:25 +0700

    add task 'CountingCharactersInFile'

--- src/javaCore/level_8/СountСharactersInFile/Solution.java
+++ src/javaCore/level_8/СountСharactersInFile/Solution.java
@@ -1,6 +1,5 @@
 package javaCore.level_8.—ount—haractersInFile;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,18 +8,13 @@ import java.util.*;
 public class Solution {
 
 	public static void main(String[] args) {
-		try(BufferedReader buffReader = new BufferedReader(new FileReader("C:\\Users\\User\\Desktop\\test.txt"))){
-			SortedMap<Character, Integer> map = new TreeMap<>();
-			String line;
-			while((line = buffReader.readLine()) != null) {
-				char charsInLine [] = line.toCharArray();
-				
-				for(int i = 0; i < charsInLine.length; i++) 
-					map.merge(charsInLine[i], 1, (a, b) -> (a+1));
-				
-				
-				map.forEach((k, v) -> System.out.println(k + " " + v));
-			}
+		try(FileReader fileReader = new FileReader(args[0])){
+			TreeMap<Character, Integer> map = new TreeMap<>();
+			int ch;
+			while((ch = fileReader.read()) != -1) 
+					map.merge((char)ch, 1, (a, b) -> (a+1));	
+			
+			map.forEach((k, v) -> System.out.println((char)k + " " + v));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
