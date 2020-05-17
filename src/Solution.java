604e0908cda698e21ad265f5edf503bab35956ab
Alexo <mordyasov.60@mail.ru>
2020-05-17 22:56:00 +0700

    add task NullObjectPattern

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,30 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
+public class Solution {
+	public static void main(String [] args) throws IOException {
+		Path file = Paths.get("C:\\Users\\Кокон2\\Desktop\\test.txt");
+		
+		Path folder = Files.createDirectory(Paths.get("ss"));
+		Path zip = Paths.get("C:\\Users\\Кокон2\\Desktop\\ДЗ.zip");
+		ZipOutputStream zipOS = new ZipOutputStream(new FileOutputStream(zip.toFile()));
+		
+		//Path directory = Paths.get(file.getParent() + "\\new");
+		//Files.copy(file, Paths.get(directory.toString() + "\\" + file.getFileName()));
+		System.out.println(folder.toAbsolutePath().toString());
+		//zipOS.putNextEntry(new ZipEntry(folder.toString() + "\\"));
+		zipOS.putNextEntry(new ZipEntry("doc.txt"));
+		Files.copy(file, zipOS);
+		
+		//zipOS.close();
+		
+		
+	}
+}
 
 	
---
+++ src/javaCollections/level_1/NullObjectPattern/FileData.java
@@ -0,0 +1,11 @@
+package javaCollections.level_1.NullObjectPattern;
+
+public interface FileData {                                                  
+    boolean isHidden();                                                  
+                                                  
+    boolean isExecutable();                                                  
+                                                  
+    boolean isDirectory();                                                  
+                                                  
+    boolean isWritable();                                                  
+}
