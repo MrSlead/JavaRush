ea9d63a942b9134072db01fcd3b8b7f597ddc65b
Alexo <mordyasov.60@mail.ru>
2020-05-25 02:20:04 +0700

    add task ReverseContentFile

--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,30 @@
-                                              
+import java.io.BufferedReader;                                                  
+import java.io.IOException;                                                  
+import java.io.InputStreamReader;
+import java.math.BigInteger;                                                  
+                                                  
+/*                                                   
+Факториал                                                  
+*/                                                  
                                                   
 public class Solution {                                                  
-    public static void main(String[] args) throws Exception {                                                  
-                                                   
+    public static void main(String[] args) throws IOException {                                                  
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));                                                  
                                                   
+        int input = Integer.parseInt(reader.readLine());                                                  
+        reader.close();                                                  
+                                                  
+        System.out.println(factorial(input));                                                  
+    }                                                  
+                                                  
+    public static String factorial(int n) {
+        if(n == 0) return String.valueOf(1);
+        else if(n < 0) return String.valueOf(0);
+        BigInteger fac = new BigInteger("1");                                             
+        for(int i = 1; i <= n; i++){
+            fac = fac.multiply(new BigInteger(String.valueOf(i)));
+        }                                                   
+                                                  
+        return String.valueOf(fac.toString());                                                  
     }                                                  
 }
--- src/javaCore/level_9/ReverseContentInFile/Solution.java
+++ src/javaCore/level_9/ReverseContentInFile/Solution.java
@@ -2,21 +2,27 @@ package javaCore.level_9.ReverseContentInFile;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Solution {
-	public static void main(String [] args) throws IOException {
-		String nameFile = "";
-		try(BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in))){
-			nameFile = buffReader.readLine();
-		}
-		
-		try(BufferedReader buffReader = new BufferedReader(new FileReader(nameFile))){
+	public static void main(String [] args) throws IOException {	
+		try(BufferedReader buffReader = new BufferedReader(new FileReader(args[0])); 
+			FileWriter fileWriter = new FileWriter(args[1])){
 			String line;
 			
 			while((line = buffReader.readLine()) != null) {
-				System.out.println(new StringBuffer(line).reverse());
+				String words [] = line.split(" ");
+				for(String s : words) {
+					if(s.length() > 6) {
+						if(!buffReader.ready() && words[words.length - 1] == s) {
+							fileWriter.write(s);
+						}
+						else {
+							fileWriter.write(s + ",");
+						}
+					}
+				}
 			}
 		}
 	}
