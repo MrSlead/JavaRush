79d297ecf0fba9e743e3f834550de9f65ba19ab6
Alexo <mordyasov.60@mail.ru>
2020-05-28 02:53:45 +0700

    add task JSON3

---
+++ src/javaCollections/level_3/JSON3/Solution.java
@@ -0,0 +1,51 @@
+package javaCollections.level_3.JSON3;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;                                                  
+                                                  
+/*                                                   
+Конвертация из одного класса в другой используя JSON Ӏ 3304                                                  
+*/                                                  
+public class Solution {                                                  
+    public static void main(String[] args) throws IOException {                                                  
+        Second s = (Second) convertOneToAnother(new First(), Second.class);                                                  
+        First f = (First) convertOneToAnother(new Second(), First.class);                                                  
+    }                                                  
+                                                  
+    public static Object convertOneToAnother(Object one, Class resultClassObject) throws IOException {
+    	 ObjectMapper mapper = new ObjectMapper();
+         String jsonString = mapper.writeValueAsString(one);
+         System.out.println(jsonString);
+         jsonString = jsonString.replaceFirst(one.getClass().getSimpleName().toLowerCase(), resultClassObject.getSimpleName().toLowerCase());
+         System.out.println(jsonString);
+         Reader reader = new StringReader(jsonString);
+         return mapper.readValue(reader, resultClassObject);                                             
+    }                                                  
+                                                  
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "className")                                                  
+    @JsonSubTypes(@JsonSubTypes.Type(value = First.class, name = "first"))                                                  
+    public static class First {                                                  
+        public int i;                                                  
+        public String name;  
+        
+        public First() {
+        	
+        }
+    }                                                  
+                                                  
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "className")                                                  
+    @JsonSubTypes(@JsonSubTypes.Type(value = Second.class, name = "second"))                                                  
+    public static class Second {                                                  
+        public int i;                                                  
+        public String name;     
+        
+        public Second() {
+        	
+        }
+    }                                                  
+}
