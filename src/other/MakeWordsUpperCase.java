c889790ff60f45c2b02ff38ee7d6c9af0659ea5d
Alex <mordyasov.60@mail.ru>
2020-07-09 21:17:00 +0700

    add task

--- src/other/MakeWordsUpperCase.java
+++ src/other/MakeWordsUpperCase.java
@@ -1,13 +1,15 @@
+package other;
+
 import java.io.BufferedReader;                                                  
 import java.io.IOException;                                                  
 import java.io.InputStreamReader;
 import java.util.Arrays;                                                  
                                                   
 /*                                                   
-–û–º–æ–≤–µ–Ω–∏–µ –†–∞–º—ã                                                  
+ŒÏÓ‚ÂÌËÂ –‡Ï˚                                                  
 */                                                  
                                                   
-public class Solution {                                                  
+public class MakeWordsUpperCase {                                                  
     public static void main(String[] args) throws IOException {                                                  
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));                                                  
         String string = reader.readLine();                                                  
@@ -20,15 +22,4 @@ public class Solution {
     }                                                  
 }                                               
                                                   
-                                                  
-                                                  
-                                                  
-                                                  
-                                                  
-                                                  
-                                                  
-                                                  
-                                                  
-                                                  
-                                                  
-                                                  
+      
